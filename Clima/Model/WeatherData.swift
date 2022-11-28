07d57c92011ce0523483aae898943c1b0fa776cb
Roger Espinoza <rj.espinoza44@gmail.com>
2022-11-27 19:51:18 -0500

    v1 - Roger Espinoza

---
+++ Clima/Model/WeatherData.swift
@@ -0,0 +1,26 @@
+//
+//  WeatherData.swift
+//  Clima
+//
+//  Created by Roger Espinoza on 26/11/22.
+//  Copyright © 2022 App Brewery. All rights reserved.
+//
+
+import Foundation
+
+struct WeatherData: Decodable {
+    let name: String
+    let main: Main
+    let weather: [Weather]
+}
+
+struct Main: Decodable {
+    let temp: Double
+}
+
+struct Weather: Decodable {
+    let description: String
+    let id: Int
+}
+
+
